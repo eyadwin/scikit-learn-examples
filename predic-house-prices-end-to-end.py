09f93223360d2eaab7f0146caa43d27c59c40249
eyadwin <dr.iyad.aboudoush@gmail.com>
2020-09-01 08:56:33 +0300

    tune the model

--- predic-house-prices-end-to-end.py
+++ predic-house-prices-end-to-end.py
@@ -453,3 +453,22 @@ from sklearn.ensemble import RandomForestRegressor
 
 forest_reg = RandomForestRegressor(n_estimators=100, random_state=42)
 forest_reg.fit(housing_prepared, housing_labels)
+
+#بامكانك استخدام GridSearchCV من Scikit-Learn للبحث بدلاً منك.
+# كل ما تحتاجه هو إخبارها المعاملات الفائقة التي تريدها أن تجربها وما هي القيم للتجربة
+# ، وسيستخدم التحقق المتبادل cross-validation لتقييم جميع التوليفات الممكنة من قيم المعاملات الفائقة
+from sklearn.model_selection import GridSearchCV
+
+param_grid = [
+    # try 12 (3×4) combinations of hyperparameters
+    {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+    # then try 6 (2×3) combinations with bootstrap set as False
+    {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]},
+  ]
+
+forest_reg = RandomForestRegressor(random_state=42)
+# train across 5 folds, that's a total of (12+6)*5=90 rounds of training
+grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
+                           scoring='neg_mean_squared_error',
+                           return_train_score=True)
+grid_search.fit(housing_prepared, housing_labels)
